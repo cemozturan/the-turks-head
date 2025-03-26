da8033566be1bd8cab5aa193a54510ab2e5f8f61
Cem Ozturan <cem.ozturan@paconsulting.com>
2025-03-26 13:28:43 +0000

    docs folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", distDir: "build" };
+const nextConfig = { output: "export", distDir: "docs" };
 
 module.exports = nextConfig;
