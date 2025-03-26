04d0b5bc84b850d653c8b75849865eb0201e976a
Cem Ozturan <cem.ozturan@paconsulting.com>
2025-03-26 13:26:10 +0000

    some config changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { output: "export", distDir: "build" };
 
-module.exports = nextConfig
+module.exports = nextConfig;
