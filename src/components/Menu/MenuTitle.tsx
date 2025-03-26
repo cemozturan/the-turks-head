d19e387acea3b36505222789eeef552bb1a30f3e
Cem Ozturan <cem.ozturan@paconsulting.com>
2025-03-26 22:01:55 +0000

    more stuff

--- src/components/Menu/MenuTitle.tsx
+++ src/components/Menu/MenuTitle.tsx
@@ -1,19 +1,14 @@
 import { TEXT_COLOR } from "@/constants/colors";
+import styles from "./MenuTitle.module.css";
 
 const MenuTitle = ({ title = "Cocktails" }: { title?: string }) => {
   return (
     <div style={{ padding: "0 5rem" }}>
       <h1
         style={{
-          fontFamily: "MyFont, sans-serif",
-          letterSpacing: "0.75px",
           color: TEXT_COLOR,
-          textAlign: "center",
-          marginTop: "-16px",
-          borderBottom: "1px solid goldenrod",
-          paddingBottom: "0.25rem",
-          marginBottom: "1rem",
         }}
+        className={styles.container}
       >
         {title}
       </h1>
